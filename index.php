3abe9316d8f26b98e51f92cb37d55de0da02d063
Dorian <150785986+EvilKroma@users.noreply.github.com>
2024-09-17 21:02:25 +0200

    Front page d'accueil presque terminé

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <main>
+        <div id="nav">
+            <span class="onglet_nav">Nous contacter</span>
+            <span class="onglet_nav">Partenaires</span>
+            <span class="onglet_nav">Informations</span>
+            <a href="account.html"><button id="btn_mon_compte">Mon compte</button></a>
+        </div>
+        <div id="image_accueil">
+            <img src="./addons/image_index.jpeg" alt="">
+        </div>
+        <div id="bandeau_recherche">
+            <h2>Enfin en vacances ?</h2>
+            <input type="text" class="champs_recherche" placeholder=" Temps sur place">
+            <input type="text" class="champs_recherche" placeholder=" Lieu">
+            <input type="text" class="champs_recherche" placeholder=" Quand">
+            <button id="btn_rechercher">Rechercher</button>
+        </div>
+        <div id="cartes_infos">
+            <div class="carte">
+                <img src="./addons/vacances.jpg" alt="">
+                <h2>Choisissez votre lieu de vacances</h2>
+            </div>
+            <div class="carte">
+                <img src="./addons/ia.jpg" alt="">
+                <h2>Laissez l'IA faire son travail</h2>
+            </div>
+            <div class="carte">
+                <img src="./addons/itinéraire.jpg" alt="">
+                <h2>Profitez de votre itinéraire !</h2>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
