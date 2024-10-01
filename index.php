694250cfc69af2974a17387d35fdf4e8e08658e2
EvilKroma <deneuchateldorian205@gmail.com>
2024-10-01 11:05:51 +0200

    Adaptation de la CD

--- index.php
+++ index.php
@@ -6,17 +6,21 @@
     <title>Dayliho</title>
     <link rel="stylesheet" href="style/style.css">
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
 <body>
     <main>
         <div id="nav">
-            <input type="text" class="champs_recherche" placeholder=" Type de sport">
-            <button id="btn_rechercher"><ion-icon name="search-outline"></ion-icon></button>
-            <span class="onglet_nav">Nous contacter</span>
-            <span class="onglet_nav">Partenaires</span>
-            <span class="onglet_nav">Informations</span>
-            <a href="account.html"><button id="btn_mon_compte">Mon compte</button></a>
+            <div id="recherche">
+                <input type="text" class="champs_recherche" placeholder=" Type de sport">
+                <button id="btn_rechercher"><ion-icon name="search-outline"></ion-icon></button>
+            </div>
+            <div class="nav-right">
+                <span class="onglet_nav">Nous contacter</span>
+                <span class="onglet_nav">Partenaires</span>
+                <span class="onglet_nav">Informations</span>
+                <a href="account.html"><button id="btn_mon_compte">Mon compte</button></a>
+            </div>
         </div>
         <div id="image_accueil">
             <img src="./addons/image_index.jpg" alt="">
@@ -24,19 +28,17 @@
         <div id="cartes_infos">
             <div class="carte">
                 <img src="./addons/vacances.jpg" alt="">
-                <h2>Choisissez votre lieu de vacances</h2>
+                <h2>Slogan carte 1</h2>
             </div>
             <div class="carte">
                 <img src="./addons/ia.jpg" alt="">
-                <h2>Laissez l'IA faire son travail</h2>
+                <h2>Slogan carte 2</h2>
             </div>
             <div class="carte">
                 <img src="./addons/itinéraire.jpg" alt="">
-                <h2>Profitez de votre itinéraire !</h2>
+                <h2>Slogan carte 3</h2>
             </div>
         </div>
     </main>
 </body>
 </html>
-
-
