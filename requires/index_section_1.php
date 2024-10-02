0c0a2907101abd1efb3b2a36a88b91fbfc68e712
FlavienNouet <flavien-nouet@outlook.fr>
2024-10-02 14:56:23 +0200

    commit

--- requires/index_section_1.php
+++ requires/index_section_1.php
@@ -3,7 +3,7 @@
     <img src="../addons/image_index.jpg" alt="">
     <div id="section_1_infos">
         <span id="entête_1"><stong>Entraînement personnel M2L</stong></span><br>
-        <span id="entête_2">Créé par des coachs, pour les joeurs.</span>
+        <span id="entête_2">Créé par des coachs, pour les joueurs.</span>
     </div>
     <div id="bouton_nous_contacter">
         <a href="">CONTACTEZ-NOUS</a>
