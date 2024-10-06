f3fc3967762134c8c1ce4b423230eeb40a161536
Dorian <150785986+EvilKroma@users.noreply.github.com>
2024-10-06 13:54:52 +0200

    Accès uniquement à index si non connecté

--- requires/nav.php
+++ requires/nav.php
@@ -4,8 +4,16 @@
     <i class="fa-solid fa-house"></i>Accueil</a>
     <a href="../pages/videos.php" class="bouton_nav">
     <i class="fa-solid fa-video"></i>Vidéos</a>
-    <a href="../pages/login.php" class="bouton_nav">
-    <i class="fa-solid fa-user"></i>Connexion</a>
-    <a href="../back/logout.php" class="bouton_nav">
-    <i class="fa-solid fa-user"></i>Déconnexion</a>
+    
+    <?php 
+        if (isset($_SESSION['user_id'])) {
+            echo '  <a href="../back/logout.php" class="bouton_nav">
+                    <i class="fa-solid fa-user"></i>Déconnexion</a>';
+        } else {
+            echo '  <a href="../pages/login.php" class="bouton_nav"> 
+                    <i class="fa-solid fa-user"></i>Connexion</a>';
+        }
+    ?>
+   
+    
 </div>
