9cb707df0c521b344e7ca67fdb3cef40c64b72d3
EvilKroma <deneuchateldorian205@gmail.com>
2024-10-29 14:37:20 +0100

    Logout nav

--- requires/nav.php
+++ requires/nav.php
@@ -9,8 +9,12 @@
     <?php 
         // Si l'utilisateur est connecté, afficher le bouton Déconnexion
         if (isset($_SESSION['user_id'])) {
-            echo '<a href="../pages/profile.php" class="bouton_nav">
-                  <i class="lev fa-solid fa-user"></i>Profile</a>';
+            echo '  <a href="../pages/profile.php" class="bouton_nav">
+                    <i class="lev fa-solid fa-user"></i>Profile</a>
+
+                    <a href="../back/logout.php" class="bouton_nav">
+                    <i class="fa-solid fa-right-from-bracket"></i></a>
+                    ';
         } else {
             // Si l'utilisateur n'est pas connecté, afficher le bouton Connexion
             echo '<a href="../pages/login.php" class="bouton_nav">
