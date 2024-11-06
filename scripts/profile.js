5e75e470e967c412e369926c737a0510d4fff359
Dorian <150785986+EvilKroma@users.noreply.github.com>
2024-11-06 14:44:26 +0100

    Erreur page profile corrigé

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,3 +1,8 @@
+// const userId = <?php echo json_encode($_SESSION['user_id']); ?>;
+const userId = 1; // TEST
+const tabs = document.querySelectorAll('.menu-item');
+const profileContent = document.getElementById('profile-content');
+
 function showAccounts() {
     profileContent.innerHTML = `
         <h2>Liste des comptes</h2>
@@ -44,6 +49,7 @@ function showAccounts() {
 function showPassword() {
     profileContent.innerHTML = `
         <h2>Changer le mot de passe</h2>
+        <h1 style="color:red;">NON FONCTIONNEL</h1>
         <form id="passwordForm">
             <div id="form-group-oldPassword" class="form-group">
                 <label for="oldPassword">Ancien mot de passe</label>
@@ -53,7 +59,7 @@ function showPassword() {
                 <label for="newPassword">Nouveau mot de passe</label>
                 <input type="password" id="newPassword" required>
             </div>
-            <button type="button" onclick="changePassword()">Changer le mot de passe</button>
+            <button type="button">Changer le mot de passe</button>
         </form>
     `;
 }
@@ -85,4 +91,36 @@ showDetails();
 
 function deleteUser(id) {
     alert(`L'ID de l'utilisateur est : ${id}`);
+}
+
+function showDetails() {
+    axios.get(`http://localhost:1234/user/getUserById?id=${userId}`)
+        .then(response => {
+            const utilisateur = response.data;
+            if (utilisateur) {
+                profileContent.innerHTML = `
+                        <h2>Account Settings</h2>
+                        <form id="profileForm">
+                            <div id="form-group-email" class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" id="email" value="${utilisateur.email}" readonly>
+                            </div>
+                            <div id="form-group-firstName" class="form-group">
+                                <label for="firstName">First name</label>
+                                <input type="text" id="firstName" value="${utilisateur.prenom}" readonly>
+                            </div>
+                            <div id="form-group-lastName" class="form-group">
+                                <label for="lastName">Last name</label>
+                                <input type="text" id="lastName" value="${utilisateur.nom}" readonly>
+                            </div>
+                        </form>
+                    `;
+            } else {
+                profileContent.innerHTML = '<p>Aucun utilisateur trouvé.</p>';
+            }
+        })
+        .catch(error => {
+            console.error("Erreur lors de la récupération des données de l'utilisateur:", error);
+            profileContent.innerHTML = '<p>Erreur lors de la récupération des données.</p>';
+        });
 }
