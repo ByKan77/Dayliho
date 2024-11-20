7cf44b72b738234a6a20f1a95a78e5224f74ef4d
Dorian <deneuchateldorian205@gmail.com>
2024-11-20 17:37:02 +0100

    database

--- API/database.js
+++ API/database.js
@@ -1,8 +1,8 @@
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({ 
     host: 'localhost',
-    user:'dorian',
-    password: 'Rootroot123.',
+    user:'root',
+    password: '',
     database: 'dayliho',
 });
 
