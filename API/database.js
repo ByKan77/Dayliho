9e056dedfe91058a6c103b26e0512fcaca908dd3
Dorian <150785986+EvilKroma@users.noreply.github.com>
2024-12-03 15:32:27 +0100

    .env

--- API/database.js
+++ API/database.js
@@ -1,9 +1,11 @@
 const mariadb = require('mariadb');
+require('dotenv').config();  // Charger les variables depuis le fichier .env
+
 const pool = mariadb.createPool({ 
-    host: 'localhost',
-    user:'root',
-    password: '',
-    database: 'dayliho',
+    host: process.env.DB_HOST,  
+    user: process.env.DB_USER,  
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME,
 });
 
 module.exports = pool;
