568bf0a593152364d7ded7f16675044f878650a0
Dorian <150785986+EvilKroma@users.noreply.github.com>
2024-12-03 15:41:19 +0100

    Revert ".env"
    This reverts commit 9e056dedfe91058a6c103b26e0512fcaca908dd3.

--- API/database.js
+++ API/database.js
@@ -1,11 +1,9 @@
 const mariadb = require('mariadb');
-require('dotenv').config();  // Charger les variables depuis le fichier .env
-
 const pool = mariadb.createPool({ 
-    host: process.env.DB_HOST,  
-    user: process.env.DB_USER,  
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_NAME,
+    host: 'localhost',
+    user:'root',
+    password: '',
+    database: 'dayliho',
 });
 
 module.exports = pool;
