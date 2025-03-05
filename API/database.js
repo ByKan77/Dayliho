383fe55e8d84a86a3979d64b8784548e76b00df9
Dorian <deneuchateldorian205@gmail.com>
2025-03-05 14:58:56 +0100

    Update database.js

--- API/database.js
+++ API/database.js
@@ -2,17 +2,10 @@ const mariadb = require('mariadb');
 require('dotenv').config()
 
 const pool = mariadb.createPool({ 
-<<<<<<< HEAD
-    host: 'localhost',
-    user:'root',
-    password: '',
-    database: 'dayliho',
-=======
     host: process.env.HOST,
     user: process.env.LOGIN,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
->>>>>>> 73124abf3c78a4774ea1f3d4efd0f5152ada0284
 });
 
 module.exports = pool;
